76e20a0adbeca55ed4b162ae5d53948e5e8c77f9
DERBALA Dani <146030030+Daani7@users.noreply.github.com>
2024-11-27 15:28:42 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const baseUrl = 'https://geo.api.gouv.fr';
+const selecRegion = document.querySelector(`#regions`);
+const selecDepartement = document.querySelector(`#departements`);
+
+fetch(`${baseUrl}/regions`)
+    .then(response => response.json())
+    .then(regions => {
+        regions.forEach(region => {
+            const option = document.createElement('option');
+            option.value = region.code;
+            option.textContent = region.nom; 
+            selecRegion.appendChild(option);
+        });
+    })
+    .catch(error => console.error('Erreur lors de la récupération des régions:', error));
+
+selecRegion.addEventListener('change', (e) => {
+    const codeRegion = e.target.value;
+    fetch(`${baseUrl}/regions/${codeRegion}/departements`)
+        .then(response => response.json())
+        .then(departements => {
+            selecDepartement.innerHTML = '<option value="">Choisissez un département</option>';
+            
+            departements.forEach(departement => {
+                const option = document.createElement('option');
+                option.value = departement.code;
+                option.textContent = departement.nom;
+                selecDepartement.appendChild(option);
+            });
+        })
+});
+
+const boutonAfficher = document.querySelector('button');
+
+boutonAfficher.addEventListener('click', () => {
+    const codeDepartement = selecDepartement.value;
+
+    if (!codeDepartement) {
+        alert("Veuillez sélectionner un département.");
+        return;
+    }
+
+    fetch(`${baseUrl}/departements/${codeDepartement}/communes`)
+        .then(response => response.json())
+        .then(communes => {
+            communes.sort((a, b) => b.population - a.population);
+
+            const communeList = document.createElement('ul');
+            communes.forEach(commune => {
+                const li = document.createElement('li');
+                li.textContent = `${commune.nom} - ${commune.population} habitants`;
+                communeList.appendChild(li);
+            });
+
+            document.body.appendChild(communeList);
+        })
+});
